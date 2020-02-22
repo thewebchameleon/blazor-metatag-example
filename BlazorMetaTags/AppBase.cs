47dc5f5396b66f3cb85c386cdf2b9c742a6b9a50
Adrian Brink <adrian.brink@outlook.com>
2020-02-22 14:05:12 +0200

    fixed malformed component error

--- BlazorMetaTags/AppBase.cs
+++ BlazorMetaTags/AppBase.cs
@@ -8,28 +8,20 @@ namespace BlazorMetaTags
     {
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            builder.OpenElement(0, "html");
-            builder.AddAttribute(1, "lang", "en");
+            builder.OpenComponent<Head>(0);
+            builder.CloseComponent();
 
-            builder.OpenElement(2, "head");
-            builder.OpenComponent<Head>(3);
+            builder.OpenElement(1, "body");
+
+            builder.OpenElement(2, "app");
+            builder.OpenComponent<App>(3);
             builder.CloseComponent();
             builder.CloseElement();
 
-            builder.OpenElement(3, "body");
-
-            builder.OpenElement(4, "app");
-            builder.OpenComponent<App>(5);
-            builder.CloseComponent();
-            builder.CloseElement();
-
-            builder.OpenComponent<Body>(6);
+            builder.OpenComponent<Body>(4);
             builder.CloseComponent();
 
-            builder.AddMarkupContent(7, " <script src='_framework/blazor.server.js'></script>");
             builder.CloseElement();
-            builder.CloseElement();
-
         }
 
     }
